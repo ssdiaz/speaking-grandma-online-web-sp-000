951e870ff224f12b02d3c75501731ee7b18ca41b
Sam D <samidiaz3@gmail.com>
2021-01-19 04:07:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase.upcase # if all uppercase (shouting)
+  if phrase == phrase.upcase # if all uppercase (shouting)
     puts "NO, NOT SINCE 1938!"
 
   elsif phrase ==  "I LOVE YOU GRANDMA!" #if phrase exactly: "I LOVE YOU GRANDMA!"
